1347a8e3b4bf3c35d4c46b32f3dd95f06b4587c5
Milly <yehm@umich.edu>
2019-07-01 17:04:11 -0400

    cleaned up code and formatting

--- app.js
+++ app.js
@@ -1,41 +1,35 @@
 
 $(document).ready(function(){
-  
-    var mousePos = {};
+    var mousePos = {}
   
    function getRandomInt(min, max) {
-     return Math.round(Math.random() * (max - min + 1)) + min;
+     return Math.round(Math.random() * (max - min + 1)) + min
    }
     
     $(window).mousemove(function(e) {
-      mousePos.x = e.pageX;
-      mousePos.y = e.pageY;
-    });
+      mousePos.x = e.pageX
+      mousePos.y = e.pageY
+    })
     
     $(window).mouseleave(function(e) {
-      mousePos.x = -1;
-      mousePos.y = -1;
-    });
+      mousePos.x = -1
+      mousePos.y = -1
+    })
     
     var draw = setInterval(function(){
       if(mousePos.x > 0 && mousePos.y > 0){
+        var range = 150
+        var color = "background: rgb("+getRandomInt(139,255)+","+getRandomInt(0,150)+","+getRandomInt(0,255)+");"
+        var sizeInt = getRandomInt(50, 70)
+        size = "height: " + sizeInt + "px; width: " + sizeInt + "px;"
         
-        var range = 150;
-        
-        var color = "background: rgb("+getRandomInt(139,255)+","+getRandomInt(0,150)+","+getRandomInt(0,255)+");";
-        
-        var sizeInt = getRandomInt(50, 70);
-        size = "height: " + sizeInt + "px; width: " + sizeInt + "px;";
-        
-        var left = "left: " + getRandomInt(mousePos.x-range-sizeInt, mousePos.x+range) + "px;";
-        
-        var top = "top: " + getRandomInt(mousePos.y-range-sizeInt, mousePos.y+range) + "px;"; 
-  
-        var style = left+top+color+size;
-        $("<div class='ball' style='" + style + "'></div>").appendTo('#wrap').one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function(){$(this).remove();}); 
+        var left = "left: " + getRandomInt(mousePos.x-range-sizeInt, mousePos.x+range) + "px;"
+        var top = "top: " + getRandomInt(mousePos.y-range-sizeInt, mousePos.y+range) + "px;"
+        var style = left+top+color+size
+        $("<div class='ball' style='" + style + "'></div>").appendTo('#wrap').one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function(){$(this).remove();})
       }
-    }, 1);
-  });
+    }, 1)
+  })
 
 let aboutButton = document.getElementsByTagName('a')[0]
 let bio = document.getElementById('bio')
